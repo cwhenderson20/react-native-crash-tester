29d4bd09a816da74f087f6eadd88a75336bc69c9
Chris Henderson <cwhenderson20@gmail.com>
2022-03-04 16:07:24 -0500

    fix: set requiresMainQueueSetup to false
    Fixes #1

--- ios/CrashTester.swift
+++ ios/CrashTester.swift
@@ -1,8 +1,13 @@
+import Foundation
+
 @objc(CrashTester)
 class CrashTester: NSObject {
-
-    @objc(nativeCrash:)
-    func nativeCrash(_ message: String = "") -> Void {
-        fatalError(message)
-    }
+  @objc static func requiresMainQueueSetup() -> Bool {
+    return false
+  }
+  
+  @objc(nativeCrash:)
+  func nativeCrash(_ message: String = "") -> Void {
+      fatalError(message)
+  }
 }
